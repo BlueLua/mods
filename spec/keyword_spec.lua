3c09bb70bc3f7c8e60776c4f013e9ecec9ea0964
Haitham <hmmelsm@gmail.com>
2026-04-26 00:49:40 +0300

    test(keyword): use each helper

--- spec/keyword_spec.lua
+++ spec/keyword_spec.lua
@@ -1,141 +1,81 @@
-local List = require "mods.List"
-local Set = require "mods.Set"
-local kw = require "mods.keyword"
-local runtime = require "mods.runtime"
+local mods = require "mods"
 
-local fmt = string.format
+local kw = mods.keyword
+local ver = mods.runtime.version
 
-describe("mods.keyword", function()
-  local ver = runtime.version
-  -- stylua: ignore
-  local keywords = List({
-    "and"   , "break" , "do"  , "else"    , "elseif",
-    "end"   , "false" , "for" , "function", "if"    ,
-    "in"    , "local" , "nil" , "not"     , "or"    ,
-    "repeat", "return", "then", "true"    , "until" , "while"
-  })
-  keywords[#keywords + 1] = ver > 501 and "goto" or nil
-  keywords[#keywords + 1] = ver > 504 and "global" or nil
-  keywords:sort()
+-- stylua: ignore
+local keywords =  mods.List({
+  "and"   , "break" , "do"  , "else"    , "elseif",
+  "end"   , "false" , "for" , "function", "if"    ,
+  "in"    , "local" , "nil" , "not"     , "or"    ,
+  "repeat", "return", "then", "true"    , "until" , "while"
+})
 
-  local kwlist = keywords
-  local kwset = keywords:toset()
-  local tests
+keywords[#keywords + 1] = ver > 501 and "goto" or nil
+keywords[#keywords + 1] = ver > 504 and "global" or nil
+keywords:sort()
 
-  -------------------
-  --- iskeyword() ---
-  -------------------
-
-  for _, input in ipairs(kwlist) do
-    it(fmt("iskeyword(%s) returns true", inspect(input)), function()
-      assert.is_true(kw.iskeyword(input))
-    end)
-  end
-
-  local non_keywords = {
-    "_",
-    "",
-    "Function",
-    "global1",
-    "goto1",
-    "hello",
-    "local_var",
-    "nil?",
-    "while_",
-    {},
-    123,
-    false,
-  }
-
-  for _, input in ipairs(non_keywords) do
-    it(fmt("iskeyword(%s) returns false", inspect(input)), function()
-      assert.is_false(kw.iskeyword(input))
-    end)
-  end
-
-  ----------------------
-  --- isidentifier() ---
-  ----------------------
-
-  -- stylua: ignore
-  tests = {
-    ------input-----|--expected---
-    { "hello"       , true      },
-    { "hello_world" , true      },
-    { "_name2"      , true      },
-    { "global"      , ver < 505 },
-    { "goto"        , ver < 502 },
-    { "(var"        , false     },
-    { "[var"        , false     },
-    { "local"       , false     },
-    { "function"    , false     },
-    { "2bad"        , false     },
-    { "bad-name"    , false     },
-    { false         , false     },
-    { nil           , false     },
-  }
-
-  for i = 1, #tests do
-    local input, expected = unpack(tests[i] --[[@as {[1]:any, [2]:boolean}]], 1, 2)
-    it(fmt("isidentifier(%s)", inspect(input)), function()
-      assert.are_equal(expected, kw.isidentifier(input))
-    end)
-  end
-
-  ------------------------------
-  --- normalize_identifier() ---
-  ------------------------------
-
-  -- stylua: ignore
-  tests = {
-    ------input------|----------------expected---------------
-    { " 2 bad-name " , "_2_bad_name"                        },
-    { "global"       , ver >= 505 and "global_" or "global" },
-    { "local"        , "local_"                             },
-    { ""             , "_"                                  },
-    { "   "          , "_"                                  },
-  }
-
-  for i = 1, #tests do
-    local input, expected = unpack(tests[i] --[[@as {[1]:string, [2]:string}]], 1, 2)
-    it(fmt("normalize_identifier(%s)", inspect(input)), function()
-      assert.are_equal(expected, kw.normalize_identifier(input))
-    end)
-  end
-
-  ----------------
-  --- kwlist() ---
-  ----------------
-
-  describe("kwlist()", function()
-    it("returns all keywords in order", function()
-      assert.are_same(kwlist, kw.kwlist())
-    end)
-
-    it("returns a mods.List instance", function()
-      assert.is_list(kw.kwlist())
-    end)
-
-    it("returns a fresh copy on each call", function()
-      assert.is_false(rawequal(kw.kwlist(), kw.kwlist()))
-    end)
-  end)
-
-  ---------------
-  --- kwset() ---
-  ---------------
-
-  describe("kwset()", function()
-    it("returns all keywords", function()
-      assert.are_same(kwset, kw.kwset())
-    end)
-
-    it("returns a mods.Set instance", function()
-      assert.is_set(kw.kwset())
-    end)
-
-    it("returns a fresh copy on each call", function()
-      assert.is_false(rawequal(kw.kwset(), kw.kwset()))
-    end)
-  end)
-end)
+-- stylua: ignore
+each("mods.keyword", kw, {
+  kwlist = {
+    {
+      expected = function()
+        assert.Same(keywords, kw.kwlist())
+        assert.List(kw.kwlist())
+        assert.False(rawequal(kw.kwlist(), kw.kwlist()))
+      end,
+    },
+  },
+  kwset = {
+    {
+      expected = function()
+        assert.Same(keywords:toset(), kw.kwset())
+        assert.Set(kw.kwset())
+        assert.False(rawequal(kw.kwset(), kw.kwset()))
+      end,
+    },
+  },
+  iskeyword= {
+    {
+      function()
+        for _, input in ipairs(keywords) do
+          assert.True(kw.iskeyword(input))
+        end
+      end,
+      { args = { "_"         }, expected = false },
+      { args = { ""          }, expected = false },
+      { args = { "Function"  }, expected = false },
+      { args = { "global1"   }, expected = false },
+      { args = { "goto1"     }, expected = false },
+      { args = { "hello"     }, expected = false },
+      { args = { "local_var" }, expected = false },
+      { args = { "nil?"      }, expected = false },
+      { args = { "while_"    }, expected = false },
+      { args = { {}          }, expected = false },
+      { args = { 123         }, expected = false },
+      { args = { false       }, expected = false },
+    },
+  },
+  isidentifier = {
+    { args = { "hello"       }, expected =  true      },
+    { args = { "hello_world" }, expected =  true      },
+    { args = { "_name2"      }, expected =  true      },
+    { args = { "global"      }, expected =  ver < 505 },
+    { args = { "goto"        }, expected =  ver < 502 },
+    { args = { "(var"        }, expected =  false     },
+    { args = { "[var"        }, expected =  false     },
+    { args = { "local"       }, expected =  false     },
+    { args = { "function"    }, expected =  false     },
+    { args = { "2bad"        }, expected =  false     },
+    { args = { "bad-name"    }, expected =  false     },
+    { args = { false         }, expected =  false     },
+    { args = { nil           }, expected =  false     },
+  },
+  normalize_identifier = {
+    { args = { " 2 bad-name " }, expected = "_2_bad_name"                        },
+    { args = { "global"       }, expected = ver >= 505 and "global_" or "global" },
+    { args = { "local"        }, expected = "local_"                             },
+    { args = { ""             }, expected = "_"                                  },
+    { args = { "   "          }, expected = "_"                                  },
+  },
+})
