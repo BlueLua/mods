3a2b1e7391fb99b260e9036b1a4fd0b8ca73646e
Haitham <hmmelsm@gmail.com>
2026-05-10 13:55:32 +0300

    test: update is

--- spec/is_spec.lua
+++ spec/is_spec.lua
@@ -5,10 +5,10 @@ local fs = mods.fs
 local is = mods.is
 local path = mods.path
 
-local fmt = string.format
 local make_tmp_dir = helpers.make_tmp_dir
 local tmpname = helpers.tmpname
 
+-- stylua: ignore
 describe("mods.is", function()
   local is_unix = not mods.runtime.is_windows
   local fn = function() end
@@ -16,62 +16,129 @@ describe("mods.is", function()
   local ct = setmetatable({}, { __call = fn })
   local nct = setmetatable({}, { __call = true })
 
-  it("is is callable", function()
-    assert.is_callable(is)
-  end)
+  -- each()
 
-  -- stylua: ignore
-  local tests = {
-    -----type----|----valid----|-----invalid----
-    { "Boolean"  , false       , 123          },
-    { "boolean"  , true        , nil          },
-    { "Function" , fn          , "abc"        },
-    { "Nil"      , nil         , 123          },
-    { "Number"   , 123         , "123"        },
-    { "String"   , "abc"       , true         },
-    { "Table"    , {}          , false        },
-    { "Thread"   , co          , fn           },
+  each("is", is, {
+    __call = {
+      { assert = function() assert.Callable(is) end        },
+      { expected = true , args = { nil      , "nil"      } },
+      { expected = true , args = { nil      , "falsy"    } },
+      { expected = false, args = { nil      , "integer"  } },
+      { expected = false, args = { nil      , "truthy"   } },
+      { expected = true , args = { false    , "boolean"  } },
+      { expected = false, args = { 123      , "boolean"  } },
+      { expected = true , args = { fn       , "function" } },
+      { expected = false, args = { "abc"    , "function" } },
+      { expected = true , args = { 123      , "number"   } },
+      { expected = false, args = { "abc"    , "number"   } },
+      { expected = true , args = { "abc"    , "string"   } },
+      { expected = false, args = { true     , "string"   } },
+      { expected = true , args = { {}       , "table"    } },
+      { expected = false, args = { false    , "table"    } },
+      { expected = true , args = { co       , "thread"   } },
+      { expected = false, args = { fn       , "thread"   } },
+      { expected = true , args = { ct       , "callable" } },
+      { expected = false, args = { nct      , "callable" } },
+      { expected = true , args = { false    , "false"    } },
+      { expected = true , args = { false    , "falsy"    } },
+      { expected = false, args = { true     , "false"    } },
+      { expected = false, args = { true     , "falsy"    } },
+      { expected = true , args = { 123      , "integer"  } },
+      { expected = false, args = { 13.4     , "integer"  } },
+      { expected = true , args = { true     , "true"     } },
+      { expected = false, args = { false    , "true"     } },
+      { expected = true , args = { 123      , "truthy"   } },
+      { expected = false, args = { false    , "truthy"   } },
+      { expected = true , args = { io.stdout, "userdata" } },
+      { expected = false, args = { {}       , "userdata" } },
 
-    { "Callable" , ct          , nct          },
-    { "callable" , fn          , {}           },
-    { "False"    , false       , true         },
-    { "falsy"    , false       , true         },
-    { "Falsy"    , nil         , 123          },
-    { "Integer"  , 123         , 13.4         },
-    { "integer"  , 123         , nil          },
-    { "true"     , true        , false        },
-    { "truthy"   , 123         , nil          },
-    { "Truthy"   , true        , false        },
-    { "userdata" , io.stdout   , {}           },
-
-    { "dir"      , "src"       , "README.md"  },
-    { "dir"      , "src"       , 123          },
-    { "file"     , "README.md" , "MISSING.md" },
-    { "file"     , "README.md" , "src"        },
-    { "file"     , "README.md" , false        },
-    { "path"     , "README.md" , "MISSING.md" },
-    { "path"     , "README.md" , false        },
-  }
-
-  for i = 1, #tests do
-    local tp, valid, invalid = unpack(tests[i] --[[@as {[1]:string, [2]:any, [3]:any}]], 1, 3)
-
-    it(fmt("is.%s(%s) returns true", tp, inspect(valid)), function()
-      assert.is_true(is[tp](valid))
-    end)
-
-    it(fmt("is.%s(%s) returns false", tp, inspect(invalid)), function()
-      assert.is_false(is[tp](invalid))
-    end)
-
-    it(fmt("is(%q, %s) returns true", tp, inspect(valid)), function()
-      assert.is_true(is(valid, tp))
-    end)
-
-    it(fmt("is(%q, %s) returns false", tp, inspect(invalid)), function()
-      assert.is_false(is(invalid, tp))
-    end)
-  end
+      -- uppercase validator names should fail (fall back to type check)
+      { expected = false, args = { nil      , "Nil"      } },
+      { expected = false, args = { false    , "Boolean"  } },
+      { expected = false, args = { "abc"    , "String"   } },
+      { expected = false, args = { 123      , "Number"   } },
+      { expected = false, args = { {}       , "Table"    } },
+      { expected = false, args = { io.stdout, "Userdata" } },
+    },
+    Boolean = {
+      { expected = true , args = { false } },
+      { expected = false, args = { 123 } },
+      { expected = false, args = { nil } },
+    },
+    Nil = {
+      { expected = true , args = { nil   } },
+      { expected = false, args = { false } },
+      { expected = false, args = { 123   } },
+    },
+    Function = {
+      { expected = true , args = { fn  } },
+      { expected = false, args = { {}  } },
+      { expected = false, args = { 123 } },
+    },
+    Falsy = {
+      { expected = true , args = { false } },
+      { expected = true , args = { nil   } },
+      { expected = false, args = { 0     } },
+      { expected = false, args = { ""    } },
+      { expected = false, args = { 123   } },
+      { expected = false, args = { "abc" } },
+      { expected = false, args = { true  } },
+    },
+    Integer = {
+      { expected = true , args = { 123   } },
+      { expected = false, args = { 13.4  } },
+      { expected = false, args = { "123" } },
+      { expected = false, args = { nil   } },
+    },
+    Truthy = {
+      { expected = true , args = { 123   } },
+      { expected = true , args = { "abc" } },
+      { expected = true , args = { true  } },
+      { expected = false, args = { false } },
+      { expected = false, args = { nil   } },
+    },
+    False = {
+      { expected = true , args = { false   } },
+      { expected = false, args = { nil     } },
+      { expected = false, args = { 0       } },
+      { expected = false, args = { "false" } },
+    },
+    True = {
+      { expected = true , args = { true   } },
+      { expected = false, args = { 1      } },
+      { expected = false, args = { "true" } },
+      { expected = false, args = { false  } },
+    },
+    Callable = {
+      { expected = true , args = { fn                                  } },
+      { expected = true , args = { setmetatable({}, { __call = fn })   } },
+      { expected = false, args = { setmetatable({}, { __call = true }) } },
+      { expected = false, args = { {}                                  } },
+      { expected = false, args = { 123                                 } },
+    },
+    Dir = {
+      { expected = true , args = { "src"       } },
+      { expected = false, args = { "README.md" } },
+      { expected = false, args = { 123         } },
+    },
+    File = {
+      { expected = true , args = { "README.md"  } },
+      { expected = false, args = { "MISSING.md" } },
+      { expected = false, args = { "src"        } },
+      { expected = false, args = { false        } },
+    },
+    Path = {
+      { expected = true , args = { "README.md"  } },
+      { expected = false, args = { "MISSING.md" } },
+      { expected = false, args = { false        } },
+    },
+    Block  = { { expected = false, args = { false       } } },
+    Char   = { { expected = false, args = { false       } } },
+    Fifo   = { { expected = false, args = { false       } } },
+    Socket = { { expected = false, args = { false       } } },
+    Device = { { expected = false, args = { false       } } },
+    Link   = { { expected = false, args = { "README.md" } } },
+  })
 
   it("link detects symlink paths when supported", function()
     local root = make_tmp_dir()
