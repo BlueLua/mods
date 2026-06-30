8ee4597d773d4e3bf675624e666b6255426f809b
Haitham <hmmelsm@gmail.com>
2026-06-30 15:42:42 +0300

    refactor(runtime): simplify module return

--- src/mods/runtime.lua
+++ src/mods/runtime.lua
@@ -1,16 +1,11 @@
 local major_s, minor_s = _VERSION:match("^Lua (%d+)%.(%d+)$")
-local major = tonumber(major_s) --[[@as integer]]
-local minor = tonumber(minor_s) --[[@as integer]]
-local is_luajit = rawget(_G, "jit") ~= nil
 local is_windows = package.config:sub(1, 1) == "\\"
 
-local M = {
-  version = major * 100 + minor,
-  major = major,
-  minor = minor,
-  is_luajit = is_luajit,
+return {
+  version = tonumber(major_s) * 100 + tonumber(minor_s),
+  major = tonumber(major_s),
+  minor = tonumber(minor_s),
+  is_luajit = rawget(_G, "jit") ~= nil,
   is_windows = is_windows,
   is_unix = not is_windows,
 }
-
-return M
